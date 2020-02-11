4a2cd5ea5a1d13827849ed6546735c4a1eb7c924
Dmitry Gurevich <dmitry.n.gurevich@gmail.com>
2020-02-11 20:57:20 +0200

    added fa icons

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const Button = ({ name, doAction }) => {
+export const Button = ({ name, icon, doAction }) => {
 
-    return <button onClick={doAction}>{name}</button>;
+    return (
+      <button onClick={doAction}>
+        {<FontAwesomeIcon icon={ icon } />}&nbsp;
+        {name}
+      </button>
+    );
 }
--- src/components/common/ListItem/ListItem.js
+++ src/components/common/ListItem/ListItem.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { Button } from '../';
+
 export const ListItem = ({ children, onItemClick, onBtnClick, ...rest }) => {
+  const deleteBtnIcon = faTrashAlt;
          return (
            <>
-           <li
-             onClick={e => onItemClick(children, e)}
-             className="list-item"
-             {...rest}
-           >
-             {children}
-           </li>
-           <Button name="Remove" doAction={() => onBtnClick(children)} />
+             <li
+               onClick={e => onItemClick(children, e)}
+               className="list-item"
+               {...rest}
+             >
+               {children}
+             </li>
+             <Button
+               name="Remove"
+               icon={deleteBtnIcon}
+               doAction={() => onBtnClick(children)}
+             />
            </>
          );
        };
